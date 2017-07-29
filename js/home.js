2e430f2ad2ad3bc46311a042aa6287df9bb42700
AlexeyErmolaev <79196495060@yandex.ru>
2017-07-29 23:30:20 +0300

    new page

---
+++ js/home.js
@@ -0,0 +1,5 @@
+$(documen).ready(function(){
+	$('.menu .container .glyphicon-search').on('click', '.menu .container .collapse .navbar-left'function(){
+		$('.menu .container .collapse .navbar-left').slideToggle();
+	});
+});
