fce6143ca14974dcf9be771e71fa4cb25f9a4466
AlexeyErmolaev <79196495060@yandex.ru>
2017-08-01 03:27:14 +0300

    add carousel

--- js/home.js
+++ js/home.js
@@ -1,5 +1,37 @@
-$(documen).ready(function(){
-	$('.menu .container .glyphicon-search').on('click', '.menu .container .collapse .navbar-left'function(){
-		$('.menu .container .collapse .navbar-left').slideToggle();
+$(document).ready(function(){
+	$('.topbutton').click( function(){ 
+		var scroll_el = $(this).attr('anchor'); 
+		if ($(scroll_el).length != 0) { 
+		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 5000); 
+		}
+		return false; 
 	});
 });
+
+$(document).ready(function() {
+    var leftclick = $('.carousel-arrow-left');
+    var rightclick = $('.carousel-arrow-right');
+	var elementsList = $('.carousel');
+ 
+	var pixelsOffset = 245;
+    var currentLeftValue = 0;
+	var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - ((elementsCount - 6) * pixelsOffset);
+    var maximumOffset = 0;
+	
+    leftclick.click(function() {
+		if (currentLeftValue != maximumOffset) {
+			currentLeftValue += 245;
+			elementsList.animate({ left : currentLeftValue + "px"}, 500);
+		}
+    });
+ 
+    rightclick.click(function() {
+		if (currentLeftValue != minimumOffset) {
+			currentLeftValue -= 245;
+			elementsList.animate({ left : currentLeftValue + "px"}, 500);
+		}
+    });
+ 
+});	
+	
